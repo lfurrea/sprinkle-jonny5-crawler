333d8b69f6ffd562584359e9d12328dd46484e9b
Luis Urrea <lfurrea@gmail.com>
2016-02-23 17:34:28 -0600

    Added proper backup before deploying

--- install.rb
+++ install.rb
@@ -18,12 +18,19 @@ package :install_beams do
   end 
 end
 
+package :backup_kazoo_dir do
+  runner "test -d #{KAZOO_DIR} && sudo tar -zcvf kazoo-config." + Time.now.to_s.gsub(':', '_').split("\s").join("_") + ".tar.gz #{KAZOO_DIR}", :sudo => true do
+    pre :install, "echo Cowardly backing up #{KAZOO_DIR}... This will always run!"
+  end
+end
+
 package :chown_beams do
   runner "chown -R #{USER}:#{GROUP} #{KAZOO_DIR}; echo done" do
   end
 end
 
 policy :jonny5_crawler, :roles => :apps do
+  requires :backup_kazoo_dir
   requires :install_beams
   requires :chown_beams
 end
